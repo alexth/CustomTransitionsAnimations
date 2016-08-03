1a86246287914647e7079ff8a4b3e55ee3054bca
Alex Golub <alexeythumb@gmail.com>
2016-08-03 23:05:42 +0300

    Add TableView

--- CustomTransitionsAnimations/CustomTransitionsAnimations/ViewController.swift
+++ CustomTransitionsAnimations/CustomTransitionsAnimations/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    let numberOfSections = 1
+    let cellIdentifier = "cellIdentifier"
+    let sourceArray = []
+
+    // MARK: View lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: TableView Data Source
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return numberOfSections
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sourceArray.count
+    }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath:indexPath) 
+        cell.textLabel?.text = sourceArray[indexPath.row] as? String
+        return cell
+    }
+
+    // MARK: TableView Delegate
+
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    }
 }
 
